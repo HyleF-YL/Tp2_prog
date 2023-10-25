81f2e6b4b3958744894d05e1ffc270c85bd1c3d8
HyleF-YL <97347658+HyleF-YL@users.noreply.github.com>
2023-10-25 16:11:43 +0200

    tp3-q2.3

---
+++ prisma/seed.ts
@@ -0,0 +1,48 @@
+import { PrismaClient, Product, ProductCategory } from "@prisma/client";
+import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
+const prisma = new PrismaClient()
+
+async function main() {
+    PRODUCTS_CATEGORY_DATA.forEach(async (product) => {
+        await prisma.productCategory.create({
+            data: {
+                id: product.id,
+                slug: product.slug,
+                name: product.name
+            }
+        })
+        product.products.forEach(async (p) => {
+            await prisma.product.create({
+                data: {
+                    id: p.id,
+                    slug: p.slug,
+                    path: p.path,
+                    name: p.name,
+                    desc: p.desc,
+                    img: p.img,
+                    price: p.price,
+                    category: {
+                        connect: {
+                            id: product.id
+                        }
+                    }
+                }
+            })
+        })
+    })
+}
+main().then(async () => {
+
+    await prisma.$disconnect()
+
+  })
+
+  .catch(async (e) => {
+
+    console.error(e)
+
+    await prisma.$disconnect()
+
+    process.exit(1)
+
+  })

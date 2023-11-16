e948611f8895a0564132328814e529472df72870
HyleF-YL <97347658+HyleF-YL@users.noreply.github.com>
2023-11-16 19:42:00 +0100

    question 3.5

--- src/app/api/product-filters/route.ts
+++ src/app/api/product-filters/route.ts
@@ -6,8 +6,15 @@ import { Product, ProductCategory } from "@prisma/client"
 
 export async function GET(req: NextRequest) {
     const params = req.nextUrl.searchParams
-    const categories = params.get("cat")?.split(",")
-    const search = params.get("search")
+    let categories = params.get("cat")?.split(",") as string[] | undefined
+    let search = params.get("search") as string | undefined
+    if(search == "undefined")
+      search = undefined
+    
+    if(categories[0] == '' || categories[0] == 'undefined')
+      categories = undefined
+
+    console.log(search)
     console.log(categories);
     
     let response = {
@@ -17,41 +24,73 @@ export async function GET(req: NextRequest) {
       },
       categories: [] as any[]
     }
-    if(categories[0] == 'undefined' || categories?.length <= 0){
-      const categories = await prisma.productCategory.findMany({
+    if(categories == undefined && search == undefined){
+      let cat = await prisma.productCategory.findMany({
         include: {
           products: true,
         }
       })
-      for(const category of categories || []) {
+      for(const category of cat || []) {
         response.categories.push(category)
         response.params.categoriesSlugs.push(category.slug)
       }
       return NextResponse.json(response)
     }
 
-    for(const category of categories || []) {
-      let cat = await prisma.productCategory.findFirst({
-        where: {
-          slug: {
-            equals: category
+    else if(categories == undefined && search != undefined){
+      let cat = await prisma.productCategory.findMany({
+        where : {
+          products: {
+            some : {
+              name: {
+                contains: search,
+                mode: "insensitive"
+              }
+            }
           }
         },
         include: {
           products: {
             where : {
               name: {
-                contains: search?.toString(),
+                contains: search,
                 mode: "insensitive"
               }
             }
           }
         }
       })
-      
-      response.categories.push(cat)
-      
-      response.params.categoriesSlugs.push(cat?.slug)
+      for(const category of cat || []) {
+        response.categories.push(category)
+        response.params.categoriesSlugs.push(category.slug)
+      }
+      return NextResponse.json(response)
     }
-    return NextResponse.json(response)
+    else{
+      for(const category of categories || [undefined]) {
+        let cat = await prisma.productCategory.findFirst({
+          where: {
+            slug: {
+              equals: category
+            }
+          },
+          
+          include: {
+            products: {
+              where : {
+                name: {
+                  contains: search,
+                  mode: "insensitive"
+                }
+              }
+            }
+          }
+        })
+        
+        response.categories.push(cat)
+        
+        response.params.categoriesSlugs.push(cat?.slug)
+        return NextResponse.json(response)
+      }
+    }
   }
--- src/components/product-list.tsx
+++ src/components/product-list.tsx
@@ -7,9 +7,7 @@ import { ProductFiltersResult } from "../types";
 import { filterProducts } from "../utils/filter-products";
 import Link from "next/link";
 import { addLine } from "../hooks/use-cart";
-import { GET } from "../app/api/product-filters/route";
-import { NextRequest } from "next/server";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Circles } from "react-loader-spinner";
 
 type Props = {
   categories: ProductsCategoryData[];
@@ -18,19 +16,20 @@ type Props = {
 
 const ProductList: FC<Props> = memo(function ({ categories, showFilters = false }) {
   const [filters, setFilters] = useState<ProductFiltersResult | undefined>();
-  const [fil, setFil] = useState<ProductFiltersResult | undefined>();
+  //const [fil, setFil] = useState<ProductFiltersResult | undefined>();
+  const [isLoading, setIsLoading] = useState(true);
   const filteredCategories = useMemo(() => filterProducts(categories, filters), [filters, categories]);
-  const [filCat, setFilCat] = useState<ProductsCategoryData | undefined>();
+  const [filCat, setFilCat] = useState<ProductsCategoryData[] | undefined>();
   useEffect(() => {
+    setIsLoading(true)
     fetch(`http://localhost:3000/api/product-filters/?search=${filters?.search}&cat=${filters?.categoriesSlugs}`, {
     method: "GET"
 
     }).then((response) => {
         return response.json()
     }).then((datas) => {
-        setFil(datas.params)
         setFilCat(datas.categories)
-        console.log(filCat);
+        setIsLoading(false)
         
     })
   }, [filters])
@@ -44,7 +43,16 @@ const ProductList: FC<Props> = memo(function ({ categories, showFilters = false 
 
       {/* Grille Produit */}
       <div className="flex-1 space-y-24">
-        {filCat?.map((cat) => (
+        {isLoading ? 
+        <Circles
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="circles-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />: filCat?.map((cat) => (
           <section key={cat.id}>
             <h2 className="text-lg font-semibold mb-8 tracking-tight">
               <Link href={`/${cat.slug}`} className="link">{cat.name} ({cat.products.length})</Link>
